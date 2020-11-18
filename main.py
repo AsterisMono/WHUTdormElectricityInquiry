6f171c66332c78ab506f5f95e7b41dc7e8a7a9c3
AsterisMono <54884471+AsterisMono@users.noreply.github.com>
2020-11-18 23:29:35 +0800

    Debug workflow with push

--- main.py
+++ main.py
@@ -69,10 +69,10 @@ if __name__ == '__main__':
     login_payload = {'logintype': 'PLATFORM', 'nickName': username, 'password': password, 'checkCode': str(intCode)}
 
     s.post(loginurl, data=login_payload)
-
+    push(s.text,apptoken,uid) # debug push
     r = requests.post(sydlapi, data=dormdata,
                       cookies=dict(JSESSIONID=s.cookies['JSESSIONID']))
-
+    push(r.text,apptoken,uid) # debug push
     if r.text[0] == '<':
         print("ERROR:Login Failed")
         quit()
